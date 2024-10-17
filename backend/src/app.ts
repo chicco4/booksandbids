70beadc5ca61134f2d42d6031ca0329236aa420b
chiccotto4 <chiccotto4@gmail.com>
2024-10-17 15:41:18 +0200

    error gestiti un po meglio

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import "dotenv/config";
 import morgan from 'morgan';
 import userRoutes from './routes/user.route';
+import createHttpError, { isHttpError } from 'http-errors';
 
 const app = express();
 
@@ -18,17 +19,19 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 
 app.use((req, res, next) => {
-    next(new Error('Endpoint not found'));
+    next(createHttpError(404, 'Endpoint not found'));
 })
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     console.error(error);
     let errorMessage = "An unknown error occurred";
-    if (error instanceof Error) {
+    let statusCode = 500;
+    if (isHttpError(error)) {
+        statusCode = error.status
         errorMessage = error.message;
     }
-    res.status(500).json({ error: errorMessage });
+    res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
