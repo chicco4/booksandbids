10d37d1619c6f64e4895a81715cd2cc25dae7c5a
chiccotto4 <chiccotto4@gmail.com>
2024-10-17 15:53:22 +0200

    updated error handling

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import UserModel from '../models/user.model';
 import userModel from '../models/user.model';
+import createHttpError from 'http-errors';
+import mongoose from 'mongoose';
 
 export const getUsers: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -16,18 +18,36 @@ export const getUser: RequestHandler = async (req, res, next) => {
   const userId = req.params.userId;
 
   try {
+    if (mongoose.isValidObjectId(userId)) {
+      throw createHttpError(400, "Invalid user ID");
+    }
+
     const user = await UserModel.findById(userId).exec();
+
+    if (!user) {
+      throw createHttpError(404, "User not found");
+    }
+
     res.status(200).json(user);
   } catch (err) {
     next(err);
   }
 };
 
-export const createUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+interface createUserBody {
+  name?: string;
+  email?: string;
+}
+
+export const createUser: RequestHandler<unknown, unknown, createUserBody, unknown> = async (req, res, next) => {
   const name = req.body.name;
   const email = req.body.email;
 
   try {
+    if (!name || !email) {
+      throw createHttpError(400, "Name and email are required");
+    }
+
     const newUser = await userModel.create({ name: name, email: email });
     res.status(201).json(newUser);
   } catch (err) {
