e98706f05ac9be111639516cd73957bfbad66eec
chiccotto4 <chiccotto4@gmail.com>
2024-10-17 15:21:37 +0200

    added getUser

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -11,6 +11,18 @@ export const getUsers: RequestHandler = async (req: Request, res: Response, next
   }
 };
 
+// using RequestHandler type, i don't need to specify the types of req, res, and next
+export const getUser: RequestHandler = async (req, res, next) => {
+  const userId = req.params.userId;
+
+  try {
+    const user = await UserModel.findById(userId).exec();
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
 export const createUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
   const name = req.body.name;
   const email = req.body.email;
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -4,6 +4,7 @@ import * as userController from '../controllers/user.controller';
 const router = express.Router();
 
 router.get('/', userController.getUsers);
+router.get('/:userId', userController.getUser);
 router.post('/', userController.createUser);
 
 export default router;
