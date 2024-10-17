8f4c5a029d3d2a0858c146a2eaeb7af303d20241
chiccotto4 <chiccotto4@gmail.com>
2024-10-17 10:49:57 +0200

    connection to db now works

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,11 @@
 // Initializes the Express app and connects the routes, middlewares, and configurations.
 // It is the main entry point for the app logic.
 import express from 'express';
-import mongoose from 'mongoose';
 import userRoutes from './routes/user.route';
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
-
-mongoose.connect('mongodb://mongo:27017/mydatabase').then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Failed to connect to MongoDB', err);
-});
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,18 @@
 // Responsible for starting the server and listening on a specified port.
 // It typically imports the app.ts and calls the listen method.
 import app from './app';
+import mongoose from 'mongoose';
 
-const PORT = 3000;
+const PORT = 4000;
+const MONGO_URI = 'mongodb://admin:adminpassword@localhost:27017/mydatabase?authSource=admin';
+
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((error) => {
+    console.error('Database connection error:', error);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
