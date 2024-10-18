f98fd35202bf811d44f7b480177f26953431f737
chiccotto4 <chiccotto4@gmail.com>
2024-10-18 13:33:04 +0200

    added email required

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -2,7 +2,7 @@ import mongoose, { InferSchemaType } from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String },
+  email: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['student', 'moderator'], required: true },
   name: { type: String },
