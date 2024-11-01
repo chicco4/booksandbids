6f74045e420d26443bba0c1f331262038718d26d
chiccotto4 <chiccotto4@gmail.com>
2024-11-01 21:46:59 +0100

    email unique

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -2,7 +2,7 @@ import mongoose, { InferSchemaType } from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true, select: false },
   name: { type: String, select: false },
   surname: { type: String, select: false },
